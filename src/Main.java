3cf49fd58bc412df29d12e34161961fd9b2ca5f3
mikseros <mmadynski@gmail.com>
2021-05-21 10:45:50 +0200

    Variables.
    Variables.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
-
+import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ArrayList<String> words = new ArrayList<String>();
+		
+		words.add("voiture");
+		words.add("bicyclette");
+		words.add("anticonstitutionnellement");
+		words.add("jazz");
+		words.add("ecole");
+		
+		int listLength = words.size();
+		int wordIndex;
+		
+		wordIndex = (int) Math.random()*listLength;
+		
+		System.out.println(wordIndex);
+		
+		wordIndex = (int) (Math.random()*listLength);
+		
+		System.out.println(wordIndex);
+
+		String selectedWord = words.get(wordIndex);
+
+		int numberOfChances = 8;
+		ArrayList<Character> chosenLetters = new ArrayList<Character>();
+		
+		
+		
+		
 
 	}
 
