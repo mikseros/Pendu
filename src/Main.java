a6bf1af9d50919b5b6a10358ac3382865505c533
Dominique Kilgour <d.kilgour@ics-nice.com>
2021-05-21 15:23:30 +0200

    methode DisplayHiddenWord

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
 import java.util.*;
 public class Main {
+	public static void DisplayHiddenWord (String selectedWord, ArrayList<Character> chosenLetters) 
+	{
+		int i=0;
+		
+		while(i<selectedWord.length()) {
+			if(chosenLetters.contains(selectedWord.charAt(i))) {
+				System.out.print(selectedWord.charAt(i));
+			}
+			else 
+				{
+				System.out.print(" _");
+				}
+			i++;
+		}
+		return;
+	}
 
 	public static void main(String[] args) {
 
-		//test23
+		//test
+		
 
 		ArrayList<String> words = new ArrayList<String>();
 		
@@ -28,10 +45,11 @@ public class Main {
 
 		int numberOfChances = 8;
 		ArrayList<Character> chosenLetters = new ArrayList<Character>();
+		chosenLetters.add('a');
+		chosenLetters.add('b');
+		String myWord= "abc";
+		DisplayHiddenWord(myWord,chosenLetters);
 		
-		
-		
-	
 
 	}
 
